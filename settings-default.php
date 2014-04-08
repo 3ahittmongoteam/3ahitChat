cf212ab0f458d5c618e2191e694b8ee54df8e6ef
ruabmbua <roland.rucky@gmail.com>
2014-04-08 16:01:53 +0200

    Added default settings file.
    Contains a skeleton for settings.php. Copy it local, and change it to
    your settings.

---
+++ settings-default.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(E_ALL);
+define('CHAT_NAME', 'CHAT_NAME');
+define('SOCKET_ADDRESS', 'SOCKET_ADDRESS');
+define('SOCKET_PORT', 'SOCKET_PORT');
+define('DB_ADDRESS', 'DB_ADDRESS');
+define('DB_PORT', 'DB_PORT');
+define('DB_USER', 'DB_USER');
+define('DB_PASSWORD', 'DB_PASSWORD');
+define('MAX_CLIENT', 'MAX_CLIENT');
+define('MAX_SIZE', 'MAX_SIZE');
+define('ALLOW_GUEST', "ALLOW_GUEST');
+?>

d076eca7b7030a791934ea73ddfe2fb4077c2877
Florian Schoeffl <florian.schoeffl@gmail.com>
2014-04-08 16:19:21 +0200

    GLobal Paths / Loggin Class / User Class Upate

--- inc/server/chat/class.user.php
+++ inc/server/chat/class.user.php
@@ -1,11 +1,26 @@
 <?php
 class User{
   private var $id;
-  private var $name;  
-  private var $ip;
   
+  public var $name;  
+  public var $ip;  
   public var $handshake;
   public var $socket;
+  
+  
+  public function __construct($socket, $name, $ip="unknown"){
+	$this->socket = $socket;
+	$this->name = $name;
+	$this->ip = $ip;
+	
+	$match = "";
+    if(preg_match("/(\d{2,4})/",(string)$socket, $match)){ $r=$match[1]; }
+    $this->id = intval($r);
+    }	
+  }
+  public function getID(){
+	return $this->id;
+  }
     
 }
 ?>
---
+++ paths.php
@@ -0,0 +1,5 @@
+<?php
+define('PROJECT_DOCUMENT_ROOT', __DIR__);
+define('PROJECT_CLASSES_PATH' , __DIR__."/inc/classes");
+define('PROJECT_INCLUDE_PATH' , __DIR__."/inc");
+?>

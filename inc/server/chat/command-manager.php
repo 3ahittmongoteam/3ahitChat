7ebfb7cdd9a3aede41bdf2cc0988fa164075d7f0
ruabmbua <roland.rucky@gmail.com>
2014-04-09 14:47:26 +0200

    Updated command manager
    The command manager is now nearly ready to use.
    Still lacking some comments in code, and interface description for
    driver implementation.

--- inc/server/chat/command-manager.php
+++ inc/server/chat/command-manager.php
@@ -1,20 +1,37 @@
 <?php
-global $commands = array();
-
-function registerCommands() {
+/**
+ * Simply call processCommand($user, $str) to use it.
+ * $user ... user context
+ * $str  ... command string(if its a command or not)
+ * The return value is false, if the command was not found / not handled.
+ * The return value is true, if a command driver handled the command successfully.
+ **/
+class CommandManager {
+	private $commands = array();
+	private $server;
 	
-}
+	public function __construct($server) {
+		$this->server = $server;
+		registerCommands();
+	}
 
-function processCommand($server, $user, $str) {
-	if(substr($str, 0, 1) != '/')
-		return false;
-	else {
-		$args = explode(" ", $str);
-		for($commands as $cmd) {
-			if($cmd->matches($args[0])) {
-				cmd->execute($server, $user, $args);
-				return true;
-			}
+	private function registerCommands() {
+		//Example:
+		//array_push($this->commands, new SampleCmdDriver());
+	}
+	
+	public function processCommand($user, $str) {
+		if(substr($str, 0, 1) == '/') {
+			$args = explode(" ", $str);
+			return processCommandArgs($user, $args);
+		}else
+			return false;
+	}
+	
+	public function processCommandArgs($user, $args) {
+		for($this->commands as $cmd) {
+			if($cmd->matches($args[0]))
+				return $cmd->execute($this->server, $user, $args);
 		}
 		return false;
 	}

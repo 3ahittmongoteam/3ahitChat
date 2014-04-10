a2a203d2c7da72a418a50de2443336ff4066d23c
ruabmbua <roland.rucky@gmail.com>
2014-04-10 12:36:49 +0200

    Added channel class
    Is not stable and compatile to ezylot code yet.

---
+++ inc/server/chat/class.channel.php
@@ -0,0 +1,28 @@
+<?php
+class Channel {
+	private static $idcounter = 0;
+	private $id;
+	private $name;
+	private $maxUser;
+	private $owner;
+	private $server;
+	
+	public function __construct($server, $name, $owner) {
+		$this->id = self::$idcounter;
+		self::$idcounter++;
+		$this->name = $name;
+		$this->owner = $owner;
+		$this->server = $server;
+	}
+	
+	//Unstable interface
+	public function removeUsers() {
+		foreach($this->server->users as $user)
+			if($user->channel == $this->id) {
+				$user->channel = 0;
+				$this->server->send($user->socket, 'CHANNEL', 'You got moved to channel ' . 
+						$this->server->channels[0]->name);
+			}
+	}
+}
+?>

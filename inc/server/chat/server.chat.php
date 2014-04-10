d809e2e22446f66559de06de6219a76031a1002d
Florian Schoeffl <florian.schoeffl@gmail.com>
2014-04-10 11:50:57 +0200

    Much changes in Server, many wow

--- inc/server/chat/server.chat.php
+++ inc/server/chat/server.chat.php
@@ -39,7 +39,7 @@ class Server(){
 					}
 				} else {
 					$bytes = @socket_recv($SingleSocket, $buffer, MAX_SIZE, 0);
-					if($bytes == 0){ disconnect($SingleSocket); }
+					if($bytes == 0){ disconnectSocket($SingleSocket); }
 					else{
 						$user = getUserBySocket($socket);
 						if(!$user->handshake){dohandshake($user,$buffer); }
@@ -57,49 +57,88 @@ class Server(){
 	}
 	
 	public function disconnectSocket($socketToDisconnect){
-		  $found=null;
-		  $n=count($users);
-		  for($i=0;$i<$n;$i++){
-			if($users[$i]->socket==$socket){ $found=$i; break; }
-		  }
-		  if(!$nomessage) { 
-			say(getuserbysocket($socket)->name." disconnected!");
-			sendall(NULL, getuserbysocket($socket)->name." disconnected!");
-		  }
-		  if(!is_null($found)){ array_splice($users,$found,1); }
-		  $index = array_search($socket,$sockets);
-		  socket_close($socket);
-		  if($index>=0){ array_splice($sockets,$index,1); }
+		if(!isset($socketToDisconnect) || empty($socketToDisconnect)) return false;
+		  
+		//Delete from User Array
+		$found=null;
+		$n=count($this->users);
+		for($i=0;$i<$n;$i++){
+			if($this->users[$i]->socket == $socketToDisconnect){ $found=$i; break; }
+		}
+		  
+		//Delete from Socket Array
+		$index = array_search($socketToDisconnect,$this->sockets);
+		socket_close($socketToDisconnect) or return false;
+		if($index >= 0 && !is_null($found)){ 
+			array_splice($this->sockets,$index,1);
+			array_splice($this->users,$found,1);	
+			return true;			
+		}
+		return false;		
 	}
 	public function disconnectId($IDToDisconnect){
-		  $found=null;
-		  $n=count($users);
-		  for($i=0;$i<$n;$i++){
-			if($users[$i]->socket==$socket){ $found=$i; break; }
-		  }
-		  if(!$nomessage) { 
-			say(getuserbysocket($socket)->name." disconnected!");
-			sendall(NULL, getuserbysocket($socket)->name." disconnected!");
-		  }
-		  if(!is_null($found)){ array_splice($users,$found,1); }
-		  $index = array_search($socket,$sockets);
-		  socket_close($socket);
-		  if($index>=0){ array_splice($sockets,$index,1); }
+		if(!isset($socketToDisconnect) || empty($socketToDisconnect)) return false;
+		  		  
+		$socketFromUser = NULL;
+		$found=null;
+		$n=count($this->users);
+		  
+		  
+		for($i=0;$i<$n;$i++){
+			if($this->users[$i]->id == $IDToDisconnect){ 
+				$found = $i; 
+				$socketFromUser = $this->users[$i]->socket; 
+				break; 
+			}
+		}
+		$index = array_search($socketFromUser,$this->sockets);
+		socket_close($socketFromUser) or return false;
+		if($index>=0 && !is_null($found)){ 
+			array_splice($this->sockets,$index,1); 
+			array_splice($this->users,$found,1);
+			return true;
+		}		
+		return false;
 	}
 	public function disconnectName($NameToDisconnect){
-		  $found=null;
-		  $n=count($users);
-		  for($i=0;$i<$n;$i++){
-			if($users[$i]->socket==$socket){ $found=$i; break; }
-		  }
-		  if(!$nomessage) { 
-			say(getuserbysocket($socket)->name." disconnected!");
-			sendall(NULL, getuserbysocket($socket)->name." disconnected!");
-		  }
-		  if(!is_null($found)){ array_splice($users,$found,1); }
-		  $index = array_search($socket,$sockets);
-		  socket_close($socket);
-		  if($index>=0){ array_splice($sockets,$index,1); }
+		if(!isset($NameToDisconnect) || empty($NameToDisconnect)) return false;
+		  		  
+		$socketFromUser = NULL;
+		$found=null;
+		$n=count($this->users);
+		  
+		  
+		for($i=0;$i<$n;$i++){
+			if($this->users[$i]->name == $NameToDisconnect){ 
+				$found = $i; 
+				$socketFromUser = $this->users[$i]->socket; 
+				break; 
+			}
+		}
+		$index = array_search($socketFromUser,$this->sockets);
+		socket_close($socketFromUser) or return false;
+		if($index>=0 && !is_null($found)){ 
+			array_splice($this->sockets,$index,1); 
+			array_splice($this->users,$found,1);
+			return true;
+		}		
+		return false;
+	}
+	public function disconnectUser($UserToDisconnect){,
+		if(!isset($UserToDisconnect) || empty($UserToDisconnect)) return false;
+		
+		$userindex = array_search($UserToDisconnect,$this->users);		
+		$socketFromUser = $this->users[$userindex]->socket;		
+		
+		$socketindex = array_search($socketFromUser,$this->sockets);		
+		socket_close($socketFromUser);
+		
+		if($userindex>=0 && $socketindex>=0){ 
+			array_splice($this->users,$userindex,1); 
+			array_splice($this->sockets,$socketindex,1);
+			return true;
+		}
+		return false;
 	}
 	
 	public function getUserBySocket($socketToFind){
@@ -129,6 +168,16 @@ class Server(){
 		return $uarr;
 	}
 	
+	//Returns a assoziativ array with the connected Users.
+	//The key of the array is the user ID and the value is the name.
+	public function getConnectedUsers(){
+		$uarr = new array();
+		foreach($this->users as $user){
+			$uarr[$user->id] = $user->name;
+		}
+		return $uarr;
+	}
+	
 	private function buildServer($address,$port){
 		if(SOCKET_MASTER_PASSWORT == ""){
 			$this->masterpasswort=NULL;
@@ -141,9 +190,50 @@ class Server(){
 		return $socket;
 	}
 	
+	private function dohandshake($user,$buffer){
+	  global $users;
+	  global $roomLimit;
+	  console("\nRequesting handshake...");
+	  list($resource,$host,$origin,$strkey,$data) = getheaders($buffer);
+	  console("Handshaking...");
+
+		$accept_key = $strkey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+		$accept_key = sha1($accept_key, true);
+		$accept_key = base64_encode($accept_key);
+
+	  $upgrade  = "HTTP/1.1 101 Switching Protocols\r\n" .
+				  "Upgrade: WebSocket\r\n" .
+				  "Connection: Upgrade\r\n" .
+				  "Sec-WebSocket-Accept: ". $accept_key . "\r\n" .
+				  "Sec-WebSocket-Origin: " . $origin . "\r\n" .
+				  "Sec-WebSocket-Location: ws://" . $host . $resource . "\r\n\r\n";
+	  socket_write($user->socket,$upgrade,strlen($upgrade));
+	  $user->handshake=true;
+	  console($upgrade);
+	  console("Done handshaking...");
+		if(count($users) > $roomLimit || count($users) > 128)
+		{
+			say("Server Room Full");
+			$tofull = "You could not connect, because the room is full(".(count($users)-1)."/".($roomLimit).")";
+			var_dump($roomLimit,count($users) );
+			send($user->socket,$tofull); 
+			return disconnect($user->socket, true);
+		}
+	  return true;
+	}
+
+	private function getheaders($req){
+	  $r=$h=$o=null;
+	  if(preg_match("/GET (.*) HTTP/"   ,$req,$match)){ $r=$match[1]; }
+	  if(preg_match("/Host: (.*)\r\n/"  ,$req,$match)){ $h=$match[1]; }
+	  if(preg_match("/Origin: (.*)\r\n/",$req,$match)){ $o=$match[1]; }
+	  if(preg_match("/Sec-WebSocket-Key: (.*)\r\n/",$req,$match)){ $key=$match[1]; }
+	  if(preg_match("/\r\n(.*?)\$/",$req,$match)){ $data=$match[1]; }
+	  return array($r,$h,$o,$key,$data);
+	}
 	
 	
 	//Gibt Text in die Console aus
-	public function say($msg=""){ echo $msg."\n"; }
+	public function console($msg=""){ echo $msg."\n"; }
 }
 ?>
